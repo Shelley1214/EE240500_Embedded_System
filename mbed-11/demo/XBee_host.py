b9736bd115b55cba78b975e9a6b4c164be924f8c
Shelley1214 <yaching1478@gmail.com>
2020-05-24 01:19:31 +0800

    mbed_11_demo

--- mbed-11/demo/XBee_host.py
+++ mbed-11/demo/XBee_host.py
@@ -5,38 +5,18 @@ import time
 serdev = '/dev/ttyUSB0'
 s = serial.Serial(serdev, 9600)
 
-prtdev = '/dev/ttyACM0'
-t = serial.Serial(prtdev, 9600)
-
 s.write("+++".encode())
-char = s.read(2)
-
 s.write("ATMY <0x140>\r\n".encode())
-char = s.read(3)
-
 s.write("ATDL <0x240>\r\n".encode())
-char = s.read(3)
-
 s.write("ATID <0x1>\r\n".encode())
-char = s.read(3)
-
 s.write("ATWR\r\n".encode())
-char = s.read(3)
-
 s.write("ATMY\r\n".encode())
-char = s.read(4)
-
 s.write("ATDL\r\n".encode())
-char = s.read(4)
-
 s.write("ATCN\r\n".encode())
-char = s.read(3)
 
 print("start sending RPC")
 
 while True:
     s.write("/getAcc/run\r".encode())
-    line=t.readline() # Read an echo string from K66F terminated with '\n' (pc.putc())
-    print(line)
     time.sleep(1)    
 s.close()
